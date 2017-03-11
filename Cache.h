3dc6f6acd59cb6dc50688c014dcc4fde9732171f
Prathamesh Prabhudesai <prathameshprabhudesai@Prathameshs-MacBook-Pro.local>
2017-03-11 02:36:51 -0600

    restructured main, changed basic architecture of cache

--- Cache.h
+++ Cache.h
@@ -55,6 +55,10 @@ class Cache {
   void setWriteBack() { writeBack = true; }
 
   void incWriteBacks(){ writeBacks++; }
+
+  void incReads() { reads++; }
+
+  void incWrites() { writes++; }
   
   void addReadMiss() { readMisses++;  missType = "READMISS"; }
 
@@ -64,7 +68,19 @@ class Cache {
 
   float getMissRate() { return(((float)(readMisses + writeMisses))/((float)(reads + writes))); } 
 
+  ulong getReads() { return reads; }
 
+  ulong getWrites() { return writes; }
+
+  ulong getReadMisses() { return readMisses; }
+
+  ulong getWriteMisses() { return writeMisses; }
+
+  ulong getWriteBacks() { return writeBacks; } 
+
+  Cache * getLowerCache() { return lowerCache; }
+
+  
   // checking bool conditions
   
   bool isWriteBack() { return writeBack; }
@@ -75,7 +91,6 @@ class Cache {
 
   bool isHit() { return (missType == "HIT"); }
 
-
   // Bit manipulations and calculations
 
   ulong getAddress(ulong tag) { return (tag << blockOffset); }
@@ -84,36 +99,44 @@ class Cache {
 
   ulong getTag(ulong address) { return (address >> (blockOffset)); }
   
-  // Fill Blocks
-
-  class CacheBlock* fillCacheBlock(ulong address){
-    int index = getIndex(address);
-    class CacheBlock* blockToBeFilled = getBlockToBeFilled(address);
-    setEvictedAddress(blockToBeFilled->getTag());
-    if (blockToBeFilled != NULL && blockToBeFilled->isDirty()) {
-      setWriteBack();
-      incWriteBacks();
+  // Try to access the block
+  
+  void getBlockAccess(ulong address, char method, bool isL2Exclusive ){
+    missType = "UNKNOWN";
+    evicted = writeBack = false;
+    class CacheBlock* CacheBlock = findCacheBlock(address);
+    if(method == 'r') incReads();  else incWrites(); 
+      
+    if(CacheBlock == NULL) {
+      if (method == 'r') addReadMiss();  else addWriteMiss();
+      if(isL2Exclusive)  CacheBlock = fillCacheBlock(address);
     }
-    if (blockToBeFilled->isValid()){
-      Set[index] = blockToBeFilled->getSeq();
-    }
-    blockToBeFilled->setTag(getTag(address));
-    blockToBeFilled->setState("VALID");
-    processLRU(blockToBeFilled);
-    return blockToBeFilled; 
+    else {
+      missType = "HIT";
+      if (REPLACEMENT_POLICY != "FIFO")  processLRU(CacheBlock);
+    } 
+    if (method == 'w')
+      CacheBlock->setState("DIRTY"); 
   }
 
-  void processLRU(class CacheBlock* cacheBlock){
-    cacheBlock->setSeq(++RPSeqNo);
+  // Find the block
+
+  class CacheBlock* findCacheBlock(ulong address){
+    ulong tag = getTag(address);
+    class CacheBlock** CacheBlock = this->CacheBlock[getIndex(address)];
+    for(int i=0; i < (int) assoc; ++i) {
+      if((tag == CacheBlock[i]->getTag()) && CacheBlock[i]->isValid())
+	return CacheBlock[i]; 
+    }
+    return NULL;
   }
-  
-  
-  class CacheBlock* getBlockToBeFilled(ulong address){
+
+  // Get The Block
+  class CacheBlock* getBlockToBeFilled(ulong address) {
     class CacheBlock* blockToBeFilled = NULL;
     ulong index = getIndex(address);
 
-    //check if there are invalid blocks in the set
-    for (int i=0; i < (int) assoc; ++i){
+    for (int i=0; i < (int) assoc; ++i) {
       if(!CacheBlock[index][i]->isValid())
 	return CacheBlock[index][i];
     }
@@ -126,45 +149,36 @@ class Cache {
 	  blockToBeFilled = CacheBlock[index][i];
 	}
       }
-    }
+    } 
     return blockToBeFilled;
   }
 
-  class CacheBlock* findCacheBlock(ulong address){
-    ulong tag = getTag(address);
-    class CacheBlock** CacheBlock = this->CacheBlock[getIndex(address)];
-    for(int i=0; i < (int) assoc; ++i) {
-      if((tag == CacheBlock[i]->getTag()) && CacheBlock[i]->isValid())
-	return CacheBlock[i]; 
+  // Fill The Block
+  
+  class CacheBlock* fillCacheBlock(ulong address){
+    int index = getIndex(address);
+    class CacheBlock* blockToBeFilled = getBlockToBeFilled(address);
+    setEvictedAddress(blockToBeFilled->getTag());
+
+    if (blockToBeFilled != NULL && blockToBeFilled->isDirty()) {
+      setWriteBack();
+      incWriteBacks();
     }
-    return NULL;
+    if (blockToBeFilled->isValid()){ Set[index] = blockToBeFilled->getSeq(); }
+
+    blockToBeFilled->setTag(getTag(address));
+    blockToBeFilled->setState("VALID");
+    processLRU(blockToBeFilled);
+    return blockToBeFilled; 
   }
 
-
-  void getBlockAccess(ulong address, char method, bool isL2Exclusive ){
-    missType = "UNKNOWN";
-    evicted = writeBack = false;
-    class CacheBlock* CacheBlock = findCacheBlock(address);
-    if(method == 'r') reads++;
-    else writes++; 
-      
-    if(CacheBlock == NULL) {
-      if (method == 'r') addReadMiss();
-      else addWriteMiss();
-      if(isL2Exclusive)
-	CacheBlock = fillCacheBlock(address);
-    }
-    else{
-      missType = "HIT";
-      if (REPLACEMENT_POLICY != "FIFO"){
-	processLRU(CacheBlock);
-      }
-    }
-    if (method == 'w')
-      CacheBlock->setState("DIRTY");
-    
+  // LRU Replacement Policy
+  void processLRU(class CacheBlock* cacheBlock) {
+    cacheBlock->setSeq(++RPSeqNo);
   }
-
+  
+  // Invalidate the block
+  
   void invalidateBlock(ulong address){
     class CacheBlock* cacheBlock = findCacheBlock(address);
     if(cacheBlock) {
@@ -178,32 +192,5 @@ class Cache {
       }
       cacheBlock->setState("INVALID");
     } 
-  }
-
-  void displayOutput(){
-    cout << "\n===== Simulation Results =====" << endl;
-    cout << "a. number of L1 reads: \t\t" << reads << endl;
-    cout << "b. number of L1 read misses: \t" << readMisses << endl;
-    cout << "c. number of L1 writes: \t" << writes << endl;
-    cout << "d. number of L1 write misses: \t" << writeMisses << endl;
-    cout << "e. L1 miss rate: \t\t" << getMissRate() << endl;
-    cout << "f. L1 writebacks: \t\t" << writeBacks << endl; 
-    if(ISL2) {
-      cout << "g. number of L2 reads: \t\t" << lowerCache->reads << endl;
-      cout << "h. number of L2 read misses: \t" << lowerCache->readMisses << endl;
-      cout << "i. number of L2 writes: \t" << lowerCache->writes << endl;
-      cout << "j. number of L2 write misses: \t" << lowerCache->writeMisses << endl;
-      cout << "k. L2 miss rate: \t\t" << lowerCache->getMissRate() << endl;
-      cout << "l. number of L2 writebacks: \t" << lowerCache->writeBacks << endl;
-      cout << "m. total memory traffic: \t" << lowerCache->getMemoryTraffic() << endl;
-    } else {
-      cout << "g. number of L2 reads: \t\t0" << endl;
-      cout << "h. number of L2 read misses: \t0" << endl;
-      cout << "i. number of L2 writes: \t0"<< endl;
-      cout << "j. number of L2 write misses: \t0" << endl;
-      cout << "k. L2 miss rate: \t\t0" << endl;
-      cout << "l. number of L2 writebacks: \t0" << endl;
-      cout << "m. total memory traffic: \t" << getMemoryTraffic() << endl;
-    }
   }
 };
--- sim_cache.cc
+++ sim_cache.cc
@@ -11,16 +11,30 @@ using namespace std;
 
 typedef unsigned long ulong;
 
-// Basic Functions assisting Main 
+//Functions assisting Main 
 
-bool readCmdInput(char *argv[],ulong *blockSize,ulong *l1Size,ulong *l1Assoc, ulong *l2Size, ulong *l2Assoc){
+void printCacheConfig(ulong blockSize, ulong l1Size, ulong l1Assoc, ulong l2Size, ulong l2Assoc,char *fname ){
+  cout << "==== Simulator Configuration ====" << endl;
+  cout << "BLOCKSIZE: \t\t" << blockSize << endl;
+  cout << "L1_SIZE: \t\t" << l1Size << endl;
+  cout << "L1_ASSOC: \t\t" << l1Assoc << endl;
+  cout << "L2_SIZE: \t\t" << l2Size << endl;
+  cout << "L1_ASSOC: \t\t" << l2Assoc << endl;
+  cout << "REPLACEMENT POLICY: \t" << REPLACEMENT_POLICY << endl;
+  cout << "INCLUSION PROPERTY: \t" << INCLUSION_TYPE << endl;
+  cout << "trace_file: \t\t" << fname;
+}
 
-  if(argv[1] == NULL) {
-    cout << "No input parameters are provided" << endl;
+// Read Command Inputs
+
+bool readCmdInput(int argc, char *argv[],ulong *blockSize,ulong *l1Size,ulong *l1Assoc, ulong *l2Size, ulong *l2Assoc, FILE ** traceFile){
+  char * fname;  
+  if(argc < 8) {
+    cout << "Not enough input parameters are provided" << endl;
     cout << "Valid Input: .\\sim_cache <BLOCKSIZE> <L1_SIZE> <L1_ASSOC> <L2_SIZE> <L2_ASSOC> <REPL_POLICY> <INCLUSION> <TRACE_FILE> " << endl;
     return false;
   }
-
+  
   *blockSize = atoi(argv[1]);
   *l1Size = atoi(argv[2]);
   *l1Assoc = atoi(argv[3]);
@@ -39,110 +53,128 @@ bool readCmdInput(char *argv[],ulong *blockSize,ulong *l1Size,ulong *l1Assoc, ul
   case 2: INCLUSION_TYPE = "EXCLUSIVE";  break;
   default: cout << "Wrong Inclusion Type: Valid Options 0: NON-INCLUSIVE, 1: INCLUSIVE, 2: EXCLUSIVE" << endl;  return false;
   }
-  
+
+  fname = argv[8]; 
+  *traceFile = fopen(fname, "r"); 
+  if(*traceFile == 0) {
+    cout << "\nThere is a problem reading the trace file." <<endl;
+    return false;
+  }
+
   ISL2 = (*l2Size == 0) ? false : true;
+  printCacheConfig(*blockSize,*l1Size,*l1Assoc,*l2Size,*l2Assoc,fname);
   return true;
 }
 
+// Output
 
-void printCacheConfig(ulong *blockSize, ulong *l1Size, ulong *l1Assoc, ulong *l2Size, ulong * l2Assoc,char fname[] ){
-  cout << "==== Simulator Configuration ====" << endl;
-  cout << "BLOCKSIZE: \t\t" << *blockSize << endl;
-  cout << "L1_SIZE: \t\t" << *l1Size << endl;
-  cout << "L1_ASSOC: \t\t" << *l1Assoc << endl;
-  cout << "L2_SIZE: \t\t" << *l2Size << endl;
-  cout << "L1_ASSOC: \t\t" << *l2Assoc << endl;
-  cout << "REPLACEMENT POLICY: \t" << REPLACEMENT_POLICY << endl;
-  cout << "INCLUSION PROPERTY: \t" << INCLUSION_TYPE << endl;
-  printf("trace_file: \t\t%s\n",fname);
+void displayOutput(Cache * L1){
+  cout << "\n===== Simulation Results =====" << endl;
+  cout << "a. number of L1 reads: \t\t" << L1->getReads() << endl;
+  cout << "b. number of L1 read misses: \t" << L1->getReadMisses() << endl;
+  cout << "c. number of L1 writes: \t" << L1->getWrites() << endl;
+  cout << "d. number of L1 write misses: \t" << L1->getWriteMisses() << endl;
+  cout << "e. L1 miss rate: \t\t" << L1->getMissRate() << endl;
+  cout << "f. number of L1 writebacks: \t" << L1->getWriteBacks() << endl; 
+  Cache * L2 = L1->getLowerCache();
+  if(L2) {
+    cout << "g. number of L2 reads: \t\t" << L2->getReads() << endl;
+    cout << "h. number of L2 read misses: \t" << L2->getReadMisses() << endl;
+    cout << "i. number of L2 writes: \t" << L2->getWrites() << endl;
+    cout << "j. number of L2 write misses: \t" << L2->getWriteMisses() << endl;
+    cout << "k. L2 miss rate: \t\t" << L2->getMissRate() << endl;
+    cout << "l. number of L2 writebacks: \t" << L2->getWriteBacks() << endl;
+    cout << "m. total memory traffic: \t" << L2->getMemoryTraffic() << endl;
+  } else {
+    cout << "g. number of L2 reads: \t\t0" << endl;
+    cout << "h. number of L2 read misses: \t0" << endl;
+    cout << "i. number of L2 writes: \t0"<< endl;
+    cout << "j. number of L2 write misses: \t0" << endl;
+    cout << "k. L2 miss rate: \t\t0" << endl;
+    cout << "l. number of L2 writebacks: \t0" << endl;
+    cout << "m. total memory traffic: \t" << L1->getMemoryTraffic() << endl;
+  }
 }
 
+// Non-Inlcusive Cache
 
-// Main Starts Here
+void nonInclusiveCache(Cache * L1, Cache * L2, ulong address) {
+  if (L1->isWriteBack())  L2->getBlockAccess(L1->getEvictedAddress(), 'w', true); 
+  if(L1->isMiss())  L2->getBlockAccess(address, 'r', true); 
+}
 
-int main(int argc, char *argv[]){
+// Inclusive Cache
 
-  bool isCmdValid, isConfigSuccessful; 
-  ulong blockSize, l1Size, l1Assoc, l2Size, l2Assoc;
-  char *fname;
-  FILE *traceFile;
-    
-  isCmdValid = readCmdInput(argv,&blockSize,&l1Size,&l1Assoc,&l2Size,&l2Assoc);
-
-  // reading the trace file
-
-  if(isCmdValid){
-    fname = argv[8];
-    traceFile = fopen(fname, "r");
-    if(traceFile == 0) {
-      cout << "\nThere is a problem reading the trace file." <<endl;
-      isConfigSuccessful = false;
-    }
-    else {
-      isConfigSuccessful = true;
-    }
+void inclusiveCache(Cache * L1, Cache * L2, ulong address) {
+  if (L1->isWriteBack())  L2->getBlockAccess(L1->getEvictedAddress(), 'w', true); 
+  if (L2->isEvicted())  L1->invalidateBlock(L2->getEvictedAddress()); 
+  if(L1->isMiss()) {
+    L2->getBlockAccess(address, 'r', true);
+    if (L2->isEvicted())  L1->invalidateBlock(L2->getEvictedAddress());
   }
-    
-  if(isConfigSuccessful) printCacheConfig(&blockSize,&l1Size,&l1Assoc,&l2Size,&l2Assoc,fname);
+}
 
-  Cache* L1 = new Cache(l1Size, l1Assoc, blockSize);
-  Cache* L2;
-  
-  if(ISL2){
-    L2 = new Cache(l2Size,l2Assoc,blockSize);
-    L1->initL2(L2);
+// Exclusive Cache
+
+void exclusiveCache(Cache * L1, Cache * L2, ulong address) {
+  if(L1->isHit()) L2->invalidateBlock(address);
+  if(L1->isEvicted()) {
+    L2->getBlockAccess(L1->getEvictedAddress(),'w',true);
+    if(!L1->isWriteBack()) L2->findCacheBlock(L1->getEvictedAddress())->setState("VALID"); 
   }
-  char instruction[20];
-  int address_accessed = 0;
-
-  /* Read the trace file */
-  while(fgets(instruction,20,traceFile) != NULL){
-    int count = 1;
-    char* token = strtok(instruction, " ");
-    char* method;
-    ulong address;
-    method = token;
-    token = strtok(NULL," ");
-    address = strtol(token,NULL,16);
-
-    L1->getBlockAccess(address,method[0],true);
-    if(ISL2){
-      // Non inclusive
-      if(INCLUSION_TYPE == "NON-INCLUSIVE") {
-	if (L1->isWriteBack())  L2->getBlockAccess(L1->getEvictedAddress(), 'w', true); 
-	if(L1->isMiss())  L2->getBlockAccess(address, 'r', true);
-      }
-      else if(INCLUSION_TYPE == "INCLUSIVE") {
-	if (L1->isWriteBack())  L2->getBlockAccess(L1->getEvictedAddress(), 'w', true); 
-	if (L2->isEvicted())  L1->invalidateBlock(L2->getEvictedAddress()); 
-	if(L1->isMiss()) {
-	  L2->getBlockAccess(address, 'r', true);
-	  if (L2->isEvicted())  L1->invalidateBlock(L2->getEvictedAddress());
-	}
-      }
-      else if(INCLUSION_TYPE == "EXCLUSIVE") {
-	if(L1->isHit()) L2->invalidateBlock(address);
-	if(L1->isEvicted()) {
-	  L2->getBlockAccess(L1->getEvictedAddress(),'w',true);
-	  if(!L1->isWriteBack()) L2->findCacheBlock(L1->getEvictedAddress())->setState("VALID"); 
-	}
-	if(L1->isMiss()){
-	  L2->getBlockAccess(address,'r',false);
-	  if(L2->isHit()){
-	    if(L2->findCacheBlock(address)->isDirty())
-	      L1->findCacheBlock(address)->setState("DIRTY");
-	    L2->invalidateBlock(address);
-	  }
-	}
-	
-      }
-      else{
-	cout << "Associativity Mismatch!!!" << endl;
+  if(L1->isMiss()) {
+    L2->getBlockAccess(address,'r',false);
+    if(L2->isHit()){
+      if(L2->findCacheBlock(address)->isDirty())  L1->findCacheBlock(address)->setState("DIRTY");
+	L2->invalidateBlock(address);
       }
     } 
   }
 
-  L1->displayOutput();
-  return 1;
-}
+  // Main Starts Here
 
+  int main(int argc, char *argv[]){
+
+    bool isCmdValid, isConfigSuccessful; 
+    ulong blockSize, l1Size, l1Assoc, l2Size, l2Assoc, address;
+    char *token, *method;
+    FILE *traceFile;
+    char instruction[20];
+  
+    isCmdValid = readCmdInput(argc,argv,&blockSize,&l1Size,&l1Assoc,&l2Size,&l2Assoc,&traceFile);
+
+    if(!isCmdValid)  return 0;
+  
+    // initialize caches
+  
+    Cache* L1 = new Cache(l1Size, l1Assoc, blockSize);
+    Cache* L2;
+
+    if(ISL2){
+      L2 = new Cache(l2Size,l2Assoc,blockSize);
+      L1->initL2(L2);
+    }
+
+    // Read the trace file and simulate cache config
+
+    while(fgets(instruction,20,traceFile) != NULL){
+      token = strtok(instruction, " "); 
+      method = token;
+      token = strtok(NULL," ");
+      address = strtol(token,NULL,16);
+
+      L1->getBlockAccess(address,method[0],true);
+
+      if(ISL2) {
+      
+	if(INCLUSION_TYPE == "NON-INCLUSIVE") nonInclusiveCache(L1, L2, address);
+
+	else if(INCLUSION_TYPE == "INCLUSIVE") inclusiveCache(L1, L2, address);
+	
+	else exclusiveCache(L1, L2, address);
+      }
+    } 
+
+    displayOutput(L1);
+    return 1;
+  }
